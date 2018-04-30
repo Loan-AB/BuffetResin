8d081ac20accecbbf622d9e0cd46acb75087027a
loanb <loanb@DESKTOP-GDOLRE8>
2018-04-30 11:40:29 +0200

    apparence

---
+++ BuffetResin/src/apparence/MonBouton.java
@@ -0,0 +1,32 @@
+package apparence;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+
+import main.*;
+
+public class MonBouton extends JButton {
+	
+	public MonBouton(String name,Boolean isopaque  )
+	{
+		
+		this.setText(name.toString());
+		this.setContentAreaFilled(false);
+		this.setFocusPainted(false);
+		
+		this.setOpaque(isopaque);
+		this.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR));
+		this.setForeground(Color.green);
+		this.setBackground(Color.LIGHT_GRAY);
+		
+		
+
+		
+	}
+	
+}
